d52c2e2372677f43de542927c102edbb81411793
mabbii <rmuneeb85@gmail.com>
2022-03-30 13:02:43 +0500

    url link

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 
@@ -14,7 +15,18 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About", list_of_numbers=[1,2,3])
+    data = []
+    with open("data/company.json", "r") as jason_data:
+        data = json.load(jason_data)
+    return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as jason_data:
+        data = json.load(jason_data)
+    
 
 
 
